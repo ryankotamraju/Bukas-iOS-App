332faa4ea0cf9649bdaf737780da99593c92d2d0
Ryan Kotamraju <kotamrajurya@gmail.com>
2024-04-29 00:44:06 -0500

    Initial Commit

---
+++ Teaching_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teaching_Project
+//
+//  Created by Ryan Kotamraju on 4/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teaching_Project/Teaching_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teaching_ProjectApp.swift
+//  Teaching_Project
+//
+//  Created by Ryan Kotamraju on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Teaching_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
